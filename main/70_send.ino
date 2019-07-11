fd112a301c32fd1ae40e75bfd5df00822d8263cc
Daniel Hilst Selli <danielhilst@gmail.com>
2019-07-11 18:03:35 -0300

    Send floats as float (not as ints)

--- main/70_send.ino
+++ main/70_send.ino
@@ -1,10 +1,28 @@
-void SendMessage(const char* topic, int value)
+void publish(const char *topic, const char *msg)
 {
-  static char charBuf[10];
-  memset(charBuf, 0, sizeof(charBuf));
-  itoa(value, charBuf, 10);
-  if (mqttcli.publish(topic, charBuf) == false) {
+  if (mqttcli.publish(topic, msg) == false)
     Serial.println("Error sending message");
-  }
+}
+
+void SendMessage(const char* topic, long int value)
+{
+  static char buf[10] = {0};
+  itoa(value, buf, 10);
+  publish(topic, buf);
+}
+
+void SendMessage(const char* topic, double value)
+{
+  static char buf[10] = {0};
+  dtostrf(value, 6, 2, buf);
+  publish(topic, buf);
+}
+
+void SendMessage(const char *topic, bool value)
+{
+  if (value)
+    publish(topic, "1");
+  else
+    publish(topic, "0");
 }
 
